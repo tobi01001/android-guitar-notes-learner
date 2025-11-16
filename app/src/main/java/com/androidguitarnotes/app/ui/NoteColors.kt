ee31b83930df3df0f6ad1699e1043d5ce1657ec7
tobi01001 <tobi931@googlemail.com>
2025-11-16 09:22:57 +0100

    Refactor: Reassign note color

--- app/src/main/java/com/androidguitarnotes/app/ui/NoteColors.kt
+++ app/src/main/java/com/androidguitarnotes/app/ui/NoteColors.kt
@@ -8,13 +8,13 @@ import androidx.compose.ui.graphics.Color
  */
 object NoteColors {
     // Natural note colors - using a color wheel approach
-    private val A_COLOR = Color(0xFFE53935) // Red
-    private val B_COLOR = Color(0xFFFF6F00) // Orange
-    private val C_COLOR = Color(0xFFFDD835) // Yellow
-    private val D_COLOR = Color(0xFF43A047) // Green
-    private val E_COLOR = Color(0xFF1E88E5) // Blue
-    private val F_COLOR = Color(0xFF5E35B1) // Purple
-    private val G_COLOR = Color(0xFFD81B60) // Pink/Magenta
+    private val A_COLOR = Color(0xFFE53935)
+    private val B_COLOR = Color(0xFF1E88E5)
+    private val C_COLOR = Color(0xFFF0F0F0)
+    private val D_COLOR = Color(0xFF5E35B1)
+    private val E_COLOR = Color(0xFFFDD835)
+    private val F_COLOR = Color(0xFFFF6F00)
+    private val G_COLOR = Color(0xFF43A047)
 
     /**
      * Returns the color for a given note name.
